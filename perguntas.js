039c0023888bcd77e5fa862bfdf39c9334c74e0a
Adamcleb <159472791+Adamcleb@users.noreply.github.com>
2024-10-17 11:07:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'filosofia',
+    'Qual filosofo Sitou a frase Deus esta morto?',
+    'Friedrich Nietzsche'
+)
+
+criaCartao(
+    'filosofia',
+    'qual filosofo ficou conhecido como o percursor da filosofia?',
+    'Socrates'
+)
+
+criaCartao(
+    'filosofia',
+    'qual o nome da primeira escola?',
+    'A academia'
+)
+criaCartao(
+    'filosofia',
+    'A onde eles moravam?',
+    'Atenas'
+)
+
+criaCartao(
+    'filosofia',
+    'SIGNIFICADO DE SINICO EM GREGO?',
+    'CÃO'
+)
